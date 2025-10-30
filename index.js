243e9b2ab32204808955e60b973c54fda7854ee0
abd-005 <abd.me7@gmail.com>
2025-10-30 22:22:50 +0600

    API Added

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 3000;
 app.get('/', (req,res)=>{
     res.send('This response is from server!');
 })
-
+users = [
+    {id:1, name:'Shabana', email:'shab@ana.com'},
+    {id:2, name:'Shabiha', email:'shab@iha.com'},
+    {id:3, name:'Shabnoor', email:'shab@noor.com'}
+]
+app.get('/users', (req,res)=>{
+    res.send(users);
+})
 app.listen(port, ()=>{
     console.log(`Users server is running on port: ${port}`)
 })
